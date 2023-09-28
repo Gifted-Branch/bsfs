0f1f07e7e4cbdcba4de2ecb63c2179193ae94013
Douglas <nkubitudouglas@gmail.com>
2023-09-28 22:45:25 +0300

    feat: Initialize App

---
+++ bsfs/bsfs/doctype/month_end_checklist/month_end_checklist.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Gifted Branch and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Month end checklist', {
+	// refresh: function(frm) {
+
+	// }
+});
