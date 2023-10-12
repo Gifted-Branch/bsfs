316ce3b162b873f74d3d0ae726efbb94d752a59f
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2023-10-12 14:34:14 +0000

    feat: Initialize App

---
+++ bsfs/bsfs/doctype/orientation_checklist_template/orientation_checklist_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Gifted Branch and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Orientation Checklist Template', {
+	// refresh: function(frm) {
+
+	// }
+});
