017e153c37ae89e1aa881d67d951fb34a3ea8a1e
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2023-10-12 17:00:00 +0000

    feat: Initialize App

---
+++ bsfs/bsfs/doctype/daily_mileage_log/daily_mileage_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Gifted Branch and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Daily Mileage Log', {
+	// refresh: function(frm) {
+
+	// }
+});
