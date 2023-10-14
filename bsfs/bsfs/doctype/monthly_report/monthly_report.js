67b09a5c3e8415776f6df00dedc6ac88271d09ee
Douglas <nkubitudouglas@gmail.com>
2023-10-14 12:04:57 +0300

    feat: Get monthly report Template items

--- bsfs/bsfs/doctype/monthly_report/monthly_report.js
+++ bsfs/bsfs/doctype/monthly_report/monthly_report.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Monthly Report', {
-	// refresh: function(frm) {
-
-	// }
+	//Get monthly report Template items
+    monthly_goals: function (frm) {
+        if (frm.doc.monthly_goals) {
+            frm.clear_table('monthly_report_detail');
+            frappe.model.with_doc('Monthly Report Template', frm.doc.monthly_goals, function () {
+                let source_doc = frappe.model.get_doc('Monthly Report Template', frm.doc.monthly_goals);
+                $.each(source_doc.monthly_report_template_detail, function (index, source_row) {
+					const target_row = frm.add_child('monthly_report_detail');
+                    target_row.goals_for_the_month = source_row.goals_month;
+                    frm.refresh_field('monthly_report_detail');
+                });
+            });
+        }
+    },
 });
