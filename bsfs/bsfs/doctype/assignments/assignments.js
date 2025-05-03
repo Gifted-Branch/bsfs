76de1516a063daf2482ef3be9aa2f9a9c550a9b5
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-05-03 08:52:05 +0300

    feat: Implement onload logic for Assignments form to manage read access based on user roles

--- bsfs/bsfs/doctype/assignments/assignments.js
+++ bsfs/bsfs/doctype/assignments/assignments.js
@@ -2,7 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Assignments', {
-	// refresh: function(frm) {
+	onload: function(frm) {
+        // Check if the current user is assigned to the task and the document is not read
+        var isAssignedUser = frm.doc.assigned_to === frappe.session.user;
+        var isManagementRole = frappe.user.has_role('Management');
 
-	// }
+        if (isAssignedUser || isManagementRole) {
+            // Set the "Read" checkbox to true (checked) in read-only mode
+            frm.set_df_property('read', 'read_only', 1);
+            frm.set_value('read', 1);
+        } else {
+            // If not assigned user or management, hide the form
+            frm.dashboard.hide();
+            frm.page.clear_primary_action();
+            frm.page.clear_menu();
+        }
+    }
 });
