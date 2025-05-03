6224a5cb05dfb23a6eadc254850cfa532236507b
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-05-03 09:36:48 +0300

    feat: Add General Note Pad DocType with associated JSON, JavaScript, Python, and test files

---
+++ bsfs/bsfs/doctype/general_note_pad/test_general_note_pad.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Gifted Branch and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGeneralNotePad(FrappeTestCase):
+	pass
